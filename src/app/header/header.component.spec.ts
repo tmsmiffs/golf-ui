e59534e01a5bee0b620cab65a6f65770a5a40f59
Marshall Smith <tmsmith@gmail.com>
2017-08-16 12:38:33 -0700

    Rearange HeaderComponenet spec

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
@@ -11,23 +11,24 @@ describe('HeaderComponent', () => {
   let de: DebugElement;
   let el: HTMLElement;
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [HeaderComponent]
-    });
+    }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
-
     comp = fixture.componentInstance;
-
     de = fixture.debugElement.query(By.css('h2'));
     el = de.nativeElement;
+
+    comp.headerText = 'Golf Stats';
+
+    fixture.detectChanges();
   });
 
   it(`should display header text in uppercase`, () => {
-    comp.headerText = 'Golf Stats';
-    fixture.detectChanges();
-
     expect(el.textContent).toEqual('GOLF STATS');
   });
 
